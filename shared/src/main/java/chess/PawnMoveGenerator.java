81dc5d788f8a2915bf23b80ede767c5c3a060604
Katia Jensen <katiajensen26@gmail.com>
2025-09-15 22:21:20 -0600

    Working on pawn move tests.

--- shared/src/main/java/chess/PawnMoveGenerator.java
+++ shared/src/main/java/chess/PawnMoveGenerator.java
@@ -2,7 +2,6 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 
 public class PawnMoveGenerator implements MoveGenerator{
 
@@ -16,6 +15,7 @@ public class PawnMoveGenerator implements MoveGenerator{
         int currentColumn = myPosition.getColumn();
 
         int direction = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? +1 : -1;
+        int[][] capture = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? new int[][]{{+1, -1}, {+1, +1}} : new int[][]{{-1, -1}, {-1, +1}};
         int startRow = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? 2 : 7;
         int lastRow = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? 8 : 1;
 
@@ -25,7 +25,7 @@ public class PawnMoveGenerator implements MoveGenerator{
         ChessPosition newPosition1 = new ChessPosition(goForward1, currentColumn);
         ChessPosition newPosition2 = new ChessPosition(goForward2, currentColumn);
 
-        if (board.getPiece(newPosition1) == null || board.getPiece(newPosition2) == null) {
+        if (board.getPiece(newPosition1) == null) {
             if (goForward1 == lastRow) {
                 //add promotion logic
             } else {
@@ -33,15 +33,34 @@ public class PawnMoveGenerator implements MoveGenerator{
                 moves.add(move);
             }
 
-            if (currentRow == startRow) {
+            if (currentRow == startRow && board.getPiece(newPosition2) == null) {
                 ChessMove move = new ChessMove(myPosition, newPosition2, null);
                 moves.add(move);
-            } else {
-                ChessMove move = new ChessMove(myPosition, newPosition1, null);
-                moves.add(move);
             }
+//            } else {
+//                ChessMove move = new ChessMove(myPosition, newPosition1, null);
+//                moves.add(move);
+//            }
         }
 
-        return List.of();
+        for (int[] cap : capture) {
+            int moveRow = currentRow + cap[0];
+            int moveColumn = currentColumn + cap[1];
+
+            if (currentRow < 1 || currentRow > 8 || currentColumn < 1 || currentColumn > 8) {
+                ChessPosition capPosition = new ChessPosition(moveRow, moveColumn);
+                ChessPiece target = board.getPiece(capPosition);
+
+                if (target != null && target.getTeamColor() != pawn.getTeamColor()) {
+                    if (moveRow == lastRow) {
+                        //promotion logic
+                    } else {
+                        ChessMove move = new ChessMove(myPosition, capPosition, null);
+                        moves.add(move);
+                    }
+                }
+            }
+        }
+        return moves;
     }
 }
