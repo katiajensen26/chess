d04f7f2bc581d8e2b50cf46cf390160707e0865a
Katia Jensen <katiajensen26@gmail.com>
2025-10-16 10:44:28 -0600

    Wrote and passed register positive test for my service.

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,25 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void register() throws Exception {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("joe", "j@J.com", "toomanysecrets");
+        var userService = new UserService(db);
+
+        var authData = userService.register(user);
+
+        assertNotNull(authData);
+        assertEquals(user.username(), authData.username());
+        assertFalse(authData.authToken().isEmpty());
+
+    }
+}
