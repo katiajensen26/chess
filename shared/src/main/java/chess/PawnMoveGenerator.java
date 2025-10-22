318d4895390cdc2b3145ead364c6acb32478dc6a
Katia Jensen <katiajensen26@gmail.com>
2025-10-22 13:11:38 -0600

    Copied over my PawnMoveGenerator code from my programming exam as I made it more efficient and wanted that to apply here.

--- shared/src/main/java/chess/PawnMoveGenerator.java
+++ shared/src/main/java/chess/PawnMoveGenerator.java
@@ -4,67 +4,64 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class PawnMoveGenerator implements MoveGenerator{
-
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new HashSet<>();
-
         ChessPiece pawn = board.getPiece(myPosition);
 
-        int currentRow = myPosition.getRow();
-        int currentColumn = myPosition.getColumn();
-
         int direction = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? +1 : -1;
-        int[][] capture = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? new int[][]{{+1, -1}, {+1, +1}} : new int[][]{{-1, -1}, {-1, +1}};
+        int[][] capture = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? new int[][]{{+1, +1}, {+1, -1}} : new int[][]{{-1, +1}, {-1, -1}};
         int startRow = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? 2 : 7;
         int lastRow = (pawn.getTeamColor() == ChessGame.TeamColor.WHITE) ? 8 : 1;
 
+        ChessPiece.PieceType[] promotionTypes = {ChessPiece.PieceType.ROOK, ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP};
+
+        int currentRow = myPosition.getRow();
+        int currentCol = myPosition.getColumn();
+
         int goForward1 = currentRow + direction;
-        int goForward2 = currentRow + direction * 2;
+        int goForward2 = currentRow + direction*2;
 
-        ChessPosition newPosition1 = new ChessPosition(goForward1, currentColumn);
-        ChessPosition newPosition2 = new ChessPosition(goForward2, currentColumn);
-
-        ChessPiece.PieceType[] promotionType = {ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.ROOK, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.KNIGHT};
+        ChessPosition newPosition1 = new ChessPosition(goForward1, currentCol);
+        ChessPosition newPosition2 = new ChessPosition(goForward2, currentCol);
 
         if (board.getPiece(newPosition1) == null) {
             if (goForward1 == lastRow) {
-                for (ChessPiece.PieceType proType : promotionType) {
-                    ChessMove move = new ChessMove(myPosition, newPosition1, proType);
-                    moves.add(move);
+                for (ChessPiece.PieceType promoType : promotionTypes) {
+                    moves.add(new ChessMove(myPosition, newPosition1, promoType));
                 }
             } else {
-                ChessMove move = new ChessMove(myPosition, newPosition1, null);
-                moves.add(move);
+                moves.add(new ChessMove(myPosition, newPosition1, null));
             }
 
-            if (currentRow == startRow && board.getPiece(newPosition2) == null) {
-                ChessMove move = new ChessMove(myPosition, newPosition2, null);
-                moves.add(move);
+            if (newPosition2.getRow() >= 1 && newPosition2.getRow() <= 8 && board.getPiece(newPosition2) == null && currentRow == startRow) {
+                moves.add(new ChessMove(myPosition, newPosition2, null));
             }
+
         }
 
         for (int[] cap : capture) {
             int moveRow = currentRow + cap[0];
-            int moveColumn = currentColumn + cap[1];
+            int moveCol = currentCol + cap[1];
 
-            if (moveRow >= 1 && moveRow <= 8 && moveColumn >= 1 && moveColumn <= 8) {
-                ChessPosition capPosition = new ChessPosition(moveRow, moveColumn);
+            if (moveRow >= 1 && moveRow <= 8 && moveCol >= 1 && moveCol <= 8) {
+
+                ChessPosition capPosition = new ChessPosition(moveRow, moveCol);
                 ChessPiece target = board.getPiece(capPosition);
 
                 if (target != null && target.getTeamColor() != pawn.getTeamColor()) {
                     if (moveRow == lastRow) {
-                        for (ChessPiece.PieceType proType : promotionType) {
-                            ChessMove move = new ChessMove(myPosition, capPosition, proType);
-                            moves.add(move);
+                        for (ChessPiece.PieceType promoType : promotionTypes) {
+                            moves.add(new ChessMove(myPosition, capPosition, promoType));
                         }
                     } else {
-                        ChessMove move = new ChessMove(myPosition, capPosition, null);
-                        moves.add(move);
+                        moves.add(new ChessMove(myPosition, capPosition, null));
                     }
                 }
             }
         }
+
+
         return moves;
     }
 }
