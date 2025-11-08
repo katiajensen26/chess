6746309157403ffbed03031c19a64a010270c456
Katia Jensen <katiajensen26@gmail.com>
2025-11-08 11:27:49 -0700

    Created a ResponseException class to handle and print out error messages.

---
+++ shared/src/main/java/server/ResponseException.java
@@ -0,0 +1,58 @@
+package server;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseException extends RuntimeException {
+
+    public enum StatusCode {
+        BadRequest,
+        Unauthorized,
+        AlreadyTaken,
+        ServerError,
+    }
+
+    final private StatusCode code;
+
+    public ResponseException(StatusCode code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = StatusCode.valueOf(map.get("status").toString());
+        String msg = map.get("message").toString();
+        return new ResponseException(status, msg);
+    }
+
+    public StatusCode code() {
+        return code;
+    }
+
+    public static StatusCode fromHttpStatus(int httpStatusCode) {
+        return switch(httpStatusCode) {
+            case 400 -> StatusCode.BadRequest;
+            case 401 -> StatusCode.Unauthorized;
+            case 403 -> StatusCode.AlreadyTaken;
+            case 500 -> StatusCode.ServerError;
+            default -> throw new IllegalArgumentException("Unknown status code: " + httpStatusCode);
+        };
+    }
+
+    public int httpStatusCode() {
+        return switch(code) {
+            case BadRequest -> 400;
+            case Unauthorized -> 401;
+            case AlreadyTaken -> 403;
+            case ServerError -> 500;
+        };
+    }
+
+}
