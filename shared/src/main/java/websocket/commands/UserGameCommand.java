81cf8667133bca2ff56ac99f913cbb60e1c1ce84
Katia Jensen <katiajensen26@gmail.com>
2025-12-02 21:10:19 -0700

    Extended UserGameCommand to accommodate for "make_move" and the extra required field.

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -18,13 +18,11 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
-    private final ChessMove move;
 
-    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
-        this.move = move;
     }
 
     public enum CommandType {
