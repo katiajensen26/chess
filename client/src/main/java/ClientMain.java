3c669b944113ff0716de451faf53a7db3804388e
Katia Jensen <katiajensen26@gmail.com>
2025-12-05 02:30:38 -0700

    Fixed nesting depth.

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -15,32 +15,29 @@ public class ClientMain {
 
         while (running) {
             try {
-                try {
-                    StarterClient starterClient = new StarterClient(serverUrl);
-                    starterClient.run();
-                    AuthData authData = starterClient.getAuthData();
-                    if (authData == null) {
-                        break;
-                    }
-                    boolean loggedIn = true;
-                    String next = "";
-                    while (loggedIn) {
-                        LoggedInClient loggedInClient = new LoggedInClient(serverUrl, authData);
-                        next = loggedInClient.run();
-                        if (next.equals("GAME")) {
-                            GameClient gameClient = new GameClient(serverUrl,
-                                    authData,
-                                    loggedInClient.getRequestedGame(),
-                                    loggedInClient.getColorState());
-                            gameClient.run();
-                        } else if (next.equals("LOGOUT")) {
-                            loggedIn = false;
-                        }
-                    }
-
-                } catch (QuitException x) {
-                    running = false;
+                StarterClient starterClient = new StarterClient(serverUrl);
+                starterClient.run();
+                AuthData authData = starterClient.getAuthData();
+                if (authData == null) {
+                    break;
                 }
+                boolean loggedIn = true;
+                String next = "";
+                while (loggedIn) {
+                    LoggedInClient loggedInClient = new LoggedInClient(serverUrl, authData);
+                    next = loggedInClient.run();
+                    if (next.equals("GAME")) {
+                        GameClient gameClient = new GameClient(serverUrl,
+                                authData,
+                                loggedInClient.getRequestedGame(),
+                                loggedInClient.getColorState());
+                        gameClient.run();
+                    } else if (next.equals("LOGOUT")) {
+                        loggedIn = false;
+                    }
+                }
+            } catch (QuitException x) {
+                    running = false;
             } catch (Throwable e) {
                 System.out.printf("Unable to start server: %s%n", e.getMessage());
             }
